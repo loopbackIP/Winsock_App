b594f32b3d3488cf56add3324a5fbc02ed5d4d48
loopbackIP <hjl3066@gmail.com>
2025-07-26 17:01:35 +0900

    초기 커밋.

---
+++ WinsockServer/server.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "¼­¹ö ½ÇÇàµÊ\n";
+    return 0;
+}
---
+++ Winsock_App/client.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "클라이언트 실행됨\n";
+    return 0;
+}
